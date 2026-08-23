1991db2b2528f0c0134f021411d790107887e6f7
Alejandro Coves Bolaños <alejandrocoves2003@gmail.com>
2026-08-23 18:18:19 +0200

    chore: gradle root skeleton + version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+                includeGroupAndSubgroups("androidx")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            content {
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+                includeGroupAndSubgroups("androidx")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ElcheApp"
+
+// FASE 1 (chunk 2): se añade include(":composeApp") en cuanto exista composeApp/build.gradle.kts
